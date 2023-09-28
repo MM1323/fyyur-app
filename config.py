3dc8fc9f0b9a46940e1d4907a807ead4732f4189
MM1323 <mia.mcduffie.13@gmail.com>
2023-09-28 12:18:41 -0400

    Adding DB config file

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+
+from settings import DB_NAME, DB_PASSWORD, DB_USER
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,4 +13,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyurdata'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
