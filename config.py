0bf4f25b47424c4a8be8d76b6cc7a88ee3064624
MM1323 <mia.mcduffie.13@gmail.com>
2023-10-06 11:01:50 -0400

    Adding database connection and Migration

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os
-
-from settings import DB_NAME, DB_PASSWORD, DB_USER
-
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,8 +8,7 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyurdata'
+SQLALCHEMY_DATABASE_URI = 'postgresql://miamcduffie:abc@localhost:5432/fyyurmusic'
 
-SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
+# SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
